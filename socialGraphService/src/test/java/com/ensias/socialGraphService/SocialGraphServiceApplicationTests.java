3b745e7f26b1bf8223a41dda01d8d80d3c0a80c5
Oussama Aarab <oussamaaarab5712@gmail.com>
2021-12-29 21:40:42 +0100

    Social Graph Service

---
+++ socialGraphService/src/test/java/com/ensias/socialGraphService/SocialGraphServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensias.socialGraphService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialGraphServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
